21820ab16690dd43b0c65d64e9effd257836d3b5
Francielle Dellamora <francielle@dellamora.dev>
2023-03-02 10:36:19 -0300

    [WIP]: create base html form

---
+++ src/components/form.jsx
@@ -0,0 +1,12 @@
+const FormComponent = () => {
+  return (
+    <form>
+      <input type="text" placeholder="New Task" id="new-task"  />
+      <button id="addBtn" type="add-task"> 
+            Add
+      </button>
+    </form>
+  );
+};
+
+export default FormComponent;
